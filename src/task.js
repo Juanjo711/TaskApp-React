a4d8690533e320bcee5d341f7d99e648d8ac64ae
Juan Estrada <juankomc711@gmail.com>
2023-07-02 14:11:00 -0500

    se crean tareas por defecto y se enlistan las tareas

---
+++ src/task.js
@@ -0,0 +1,18 @@
+// Arreglo con la lista de tareas por defecto
+export const tasks = [
+	{
+		id: 1,
+		title: "My first task",
+		description: "Description of my first task",
+	},
+	{
+		id: 2,
+		title: "My second task",
+		description: "Description of my second task",
+	},
+	{
+		id: 3,
+		title: "My third task",
+		description: "Description of my third task",
+	},
+];
